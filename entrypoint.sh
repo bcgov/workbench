1c15f63be87cd33a0f2ea8d13e9f04beb502de71
Laurent Goderre <laurent.goderre@gmail.com>
2019-06-07 18:25:39 +0000

    Fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 mkdir -p ./config
-printf "{\n" > ./config/default.json
-printf "\"logEnvironment\": \"${LOG_LEVEL}\",\n" >> ./config/default.json
-printf "\"serverPort\": 9000,\n" >> ./config/default.json
+printf '{
+    "logEnvironment": "'${LOG_LEVEL}'",
+    "serverPort": 9000,
 
-printf "\"sessionSecret\": \"${SESSION_SECRET}\",\n" >> ./config/default.json
-printf "\"sessionCookieSecure\": ${SESSION_COOKIE_SECURE},\n" >> ./config/default.json
+    "sessionSecret": "'${SESSION_SECRET}'",
+    "sessionCookieSecure": "'${SESSION_COOKIE_SECURE}'",
 
-printf "\"oidc\": {\n" >> ./config/default.json
-printf "\"issuer\": \"${OIDC_ISSUER}\",\n" >> ./config/default.json
-printf "\"clientID\": \"${OIDC_CLIENT_ID}\",\n" >> ./config/default.json
-printf "\"clientSecret\": \"${OIDC_CLIENT_SECRET}\",\n" >> ./config/default.json
-printf "\"authorizationEndpoint\": \"${OIDC_AUTH_URL}\",\n" >> ./config/default.json
-printf "\"tokenEndpoint\": \"${OIDC_TOKEN_URL}\",\n" >> ./config/default.json
-printf "\"userInfoURL\": \"${OIDC_USERINFO_URL}\",\n" >> ./config/default.json
-printf "\"callbackURL\": \"${OIDC_CALLBACK_URL}\",\n" >> ./config/default.json
-printf "\"scope\": \"${OIDC_SCOPE}\"\n" >> ./config/default.json
-printf "}\n" >> ./config/default.json
-printf "}" >> ./config/default.json
+    "oidc": {
+        "issuer": "'${OIDC_ISSUER}'",
+        "clientID": "'${OIDC_CLIENT_ID}'",
+        "clientSecret": "'${OIDC_CLIENT_SECRET}'",
+        "authorizationEndpoint": "'${OIDC_AUTH_URL}'",
+        "tokenEndpoint": "'${OIDC_TOKEN_URL}'",
+        "userInfoURL": "'${OIDC_USERINFO_URL}'",
+        "callbackURL": "'${OIDC_CALLBACK_URL}'",
+        "scope": "'${OIDC_SCOPE}'"
+    }
+}' >> ./config/default.json
 
 yarn start:prod
